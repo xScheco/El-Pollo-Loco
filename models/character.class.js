43928c0511f00b24f7aa8fa9295bd820c62ea84d
MarcoScherf <scheco13@gmail.com>
2022-06-24 20:26:46 +0200

    add more background to x-axis

--- models/character.class.js
+++ models/character.class.js
@@ -20,11 +20,17 @@ class Character extends MovableObject {
         setInterval(() => {
             if (this.world.keyboard.RIGHT) {
                 this.x += this.speed;
+                this.otherDirection = false;
             }
             if (this.world.keyboard.LEFT) {
                 this.x -= this.speed;
+                this.otherDirection = true;
+
             }
+            this.world.camera_x = -this.x;
         }, 1000 / 60);
+
+
         setInterval(() => {
             if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
                 let i = this.currentImage % this.IMAGE_WALKING.length;
--- models/world.class.js
+++ models/world.class.js
@@ -11,15 +11,38 @@ class World {
         new Cloud(),
     ];
     backgroundObject = [
-        new BackgroundObject('img/5.Fondo/Capas/5.cielo_1920-1080px.png', 0),
+        new BackgroundObject('img/5.Fondo/Capas/5.cielo_1920-1080px.png', -719), // 0 = x-achse
+        new BackgroundObject('img/5.Fondo/Capas/3.Fondo3/2.png', -719),
+        new BackgroundObject('img/5.Fondo/Capas/2.Fondo2/2.png', -719),
+        new BackgroundObject('img/5.Fondo/Capas/1.suelo-fondo1/2.png', -719),
+
+        new BackgroundObject('img/5.Fondo/Capas/5.cielo_1920-1080px.png', 0), // 0 = x-achse
         new BackgroundObject('img/5.Fondo/Capas/3.Fondo3/1.png', 0),
         new BackgroundObject('img/5.Fondo/Capas/2.Fondo2/1.png', 0),
-        new BackgroundObject('img/5.Fondo/Capas/1.suelo-fondo1/1.png', 0)
+        new BackgroundObject('img/5.Fondo/Capas/1.suelo-fondo1/1.png', 0),
+
+        new BackgroundObject('img/5.Fondo/Capas/5.cielo_1920-1080px.png', 719), // 720 = x-achse
+        new BackgroundObject('img/5.Fondo/Capas/3.Fondo3/2.png', 719),
+        new BackgroundObject('img/5.Fondo/Capas/2.Fondo2/2.png', 719),
+        new BackgroundObject('img/5.Fondo/Capas/1.suelo-fondo1/2.png', 719),
+
+        new BackgroundObject('img/5.Fondo/Capas/5.cielo_1920-1080px.png', 1438), // 720 = x-achse
+        new BackgroundObject('img/5.Fondo/Capas/3.Fondo3/1.png', 1438),
+        new BackgroundObject('img/5.Fondo/Capas/2.Fondo2/1.png', 1438),
+        new BackgroundObject('img/5.Fondo/Capas/1.suelo-fondo1/1.png', 1438),
+
+        new BackgroundObject('img/5.Fondo/Capas/5.cielo_1920-1080px.png', 719 * 3), // 720 = x-achse
+        new BackgroundObject('img/5.Fondo/Capas/3.Fondo3/2.png', 719 * 3),
+        new BackgroundObject('img/5.Fondo/Capas/2.Fondo2/2.png', 719 * 3),
+        new BackgroundObject('img/5.Fondo/Capas/1.suelo-fondo1/2.png', 719 * 3)
 
     ]
     canvas;
     ctx;
     keyboard;
+    camera_x = 0;
+
+
     constructor(canvas, keyboard) {
         this.ctx = canvas.getContext('2d');
         this.canvas = canvas;
@@ -34,10 +57,12 @@ class World {
 
     draw() {
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        this.ctx.translate(this.camera_x, 0); // background wird um 100px nach links verschoben
         this.addObjectsToMap(this.backgroundObject);
         this.addObjectsToMap(this.clouds);
         this.addToMap(this.character);
         this.addObjectsToMap(this.enemies);
+        this.ctx.translate(-this.camera_x, 0); // background wird um 100px wieder nach rechts verschoben
 
 
 
@@ -56,6 +81,16 @@ class World {
     }
 
     addToMap(mo) {
+        if (mo.otherDirection) {
+            this.ctx.save();
+            this.ctx.translate(mo.width, 0);
+            this.ctx.scale(-1, 1);
+            mo.x = mo.x * -1;
+        }
         this.ctx.drawImage(mo.img, mo.x, mo.y, mo.width, mo.height);
+        if (mo.otherDirection) {
+            mo.x = mo.x * -1;
+            this.ctx.restore();
+        }
     }
 }
